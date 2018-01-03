375784d3cca9e3e1291ae078cdd74c5cf9075c03
Josh Beard <josh@jlbeard.com>
2018-01-02 23:30:17 -0500

    Split block color into separate class

--- iosTris/iosTris/Block.swift
+++ iosTris/iosTris/Block.swift
@@ -8,37 +8,6 @@
 
 import SpriteKit
 
-let NumberOfColors: UInt32 = 6
-
-enum BlockColor: Int, CustomStringConvertible {
-    case Blue = 0, Orange, Purple, Red, Teal, Yellow
-
-    var spriteName: String {
-        switch self {
-        case .Blue:
-            return "blue"
-        case .Orange:
-            return "orange"
-        case .Purple:
-            return "purple"
-        case .Red:
-            return "red"
-        case .Teal:
-            return "teal"
-        case .Yellow:
-            return "yellow"
-        }
-    }
-    
-    var description: String {
-        return self.spriteName
-    }
-    
-    static func random() -> BlockColor {
-        return BlockColor(rawValue: Int(arc4random_uniform(NumberOfColors)))!
-    }
-}
-
 class Block: Hashable, CustomStringConvertible {
     let color: BlockColor
     
---
+++ iosTris/iosTris/BlockColor.swift
@@ -0,0 +1,40 @@
+//
+//  BlockColor.swift
+//  iosTris
+//
+//  Created by Joshua Beard on 1/2/18.
+//  Copyright © 2018 Joshua Beard. All rights reserved.
+//
+
+import SpriteKit
+
+let NumberOfColors: UInt32 = 6
+
+enum BlockColor: Int, CustomStringConvertible {
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    var spriteName: String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    var description: String {
+        return self.spriteName
+    }
+    
+    static func random() -> BlockColor {
+        return BlockColor(rawValue: Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
